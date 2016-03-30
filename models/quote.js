567e04eaa953335352b5a17cd2b014684288c9ba
odholden <odholden@gmail.com>
2016-03-30 13:39:57 +0100

    restarted and removed initial template

--- models/quote.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var quoteSchema = mongoose.Schema({
-  author: String,
-  content: String,
-  image: String
-})
-
-module.exports = mongoose.model("Quote", quoteSchema);
